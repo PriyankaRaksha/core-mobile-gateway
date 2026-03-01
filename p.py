7b10fb5bd918ee69762d45dfacd7d5f55361507d
Arjun <arjun@bcits.com>
2026-03-01 14:49:17 +0530

    delete all

--- p.py
+++
@@ -1,8 +0,0 @@
-print("Hello")
-
-a=4
-b=5
-if a>b :
-    print("Yes")
-else:
-    print("No")
--- p2.py
+++
@@ -1,9 +0,0 @@
-print("Hello Everyone")
-
-a = 5
-if(a>50):
-    print("Age is greater than 50")
-else:
-    print("Age is not greater than 50")
-
-   
